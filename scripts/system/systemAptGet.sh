9c158e86aa9c787e39e0e56b1fdbbe46295d1670
Abraham Arce <xe1gyq@gmail.com>
2013-07-27 13:46:23 -0500

    Scripts: System AptGet Ax25
    Include AX.25 packages
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>
    Signed-off-by: Ricardo Perez Olivares <xe1gyp@gmail.com>

--- scripts/system/systemAptGet.sh
+++ scripts/system/systemAptGet.sh
@@ -13,9 +13,15 @@ export aptgetCommandInstall=install
 # =============================================================================
 
 export packageGit=git
+
 export packageKernelPackage=kernel-package
 export packageLibNCurses5=libncurses5
 export packageLibNCursesDev=libncurses-dev
+
+export packageAx25Tools=ax25-tools
+export packageAx25Xtools=ax25-xtools
+export packageAx25Apps=ax25-apps
+export packageLibAx25Dev=libax25-dev
 
 # =============================================================================
 # Script Functions
@@ -37,8 +43,14 @@ aptgetFunctionInstall() {
 aptgetFunctionUpdate
 
 aptgetFunctionInstall $packageGit
+
 aptgetFunctionInstall $packageKernelPackage
 aptgetFunctionInstall $packageLibNCurses5
 aptgetFunctionInstall $packageLibNCursesDev
+
+aptgetFunctionInstall $packageAx25Tools
+aptgetFunctionInstall $packageAx25Xtools
+aptgetFunctionInstall $packageAx25Apps
+aptgetFunctionInstall $packageLibAx25Dev
 
 # End of file
