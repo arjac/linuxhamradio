1cb61d60356aeaeeec30c1245d06f3e81fd7803b
Abraham Arce <xe1gyq@gmail.com>
2013-07-27 22:30:19 -0500

    UserSpace: System Git Directories Definition
    We need to include absoluth paths for directories, let's start with
    
    - add variables to define the absoluth path of the directories:
      scripts
      scripts/system
      scripts/application
      home/<user>/reposgit (where we will clone our git repositories)
    - modify functions to handle those new variables + additional functionality
      gitFunctionVerify
      gitFunctionClone
      gitFunctionPull
    - update all variables in the rest of the script
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>
    Signed-off-by: Ricardo Perez Olivares <xe1gyp@gmail.com>

--- scripts/system/systemGit.sh
+++ scripts/system/systemGit.sh
@@ -4,6 +4,11 @@
 # Scripts Variables General
 # =============================================================================
 
+export directorySystem=`pwd`
+export directoryScripts=$directorySystem/../
+export directoryApplications=$directorySystem/../applications
+export directoryRepositories=/home/`whoami`/reposgit
+
 export systemGit=`basename $0`
 export systemGitArgumentTree=$1
 
@@ -19,9 +24,11 @@ export gitRepositoryAddress=""
 # =============================================================================
 
 export repositoryUemacsName=uemacs
+export repositoryUemacsDirectory=$directoryRepositories/$repositoryUemacsName
 export repositoryUemacsAddress=git://git.kernel.org/pub/scm/editors/uemacs/uemacs.git
 
 export repositoryLinuxKernelName=linux
+export repositoryLinuxKernelDirectory=$directoryRepositories/$repositoryLinuxKernelName
 export repositoryLinuxKernelAddress=git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
 
 
@@ -30,24 +37,25 @@ export repositoryLinuxKernelAddress=git://git.kernel.org/pub/scm/linux/kernel/gi
 # =============================================================================
 
 gitFunctionVerify() {
-        repositoryName=$1
-        test -d /home/`whoami`/$repositoryName
+        repositoryDirectory=$1
+        test -d $repositoryDirectory
 	if [ $? -eq 1 ]
 	then
-		echo "${systemGit}: Cannot find ${repositoryName} directory"
+		echo "${systemGit}: Cannot find ${repositoryDirectory} directory"
 		return 1
 	fi
 }
 
 gitFunctionClone() {
 	repositoryAddress=$1
-	cd /home/`whoami`/
+	mkdir -p $directoryRepositories
+	cd $directoryRepositories
 	$gitName $gitCommandClone $repositoryAddress
 }
 
 gitFunctionPull() {
-        repositoryName=$1
-	cd /home/`whoami`/$repositoryName
+        repositoryDirectory=$1
+	cd $repositoryDirectory
 	$gitName $gitCommandPull
 }
 
@@ -56,19 +64,22 @@ gitFunctionPull() {
 # =============================================================================
 
 if [ "$systemGitArgumentTree" = "uemacs" ]; then
-	gitRepositoryName=$repositoryUemacsName
+	gitRepositoryDirectory=$repositoryUemacsDirectory
 	gitRepositoryAddress=$repositoryUemacsAddress
 elif [ "$systemGitArgumentTree" = "linuxkernel" ]; then
-        gitRepositoryName=$repositoryLinuxKernelName
+        gitRepositoryDirectory=$repositoryLinuxKernelDirectory
         gitRepositoryAddress=$repositoryLinuxKernelAddress
+else
+	echo "${systemGit}: Cannot find project to work with, wanna use our variables?"
+	return 1
 fi
 
-gitFunctionVerify $gitRepositoryName
+gitFunctionVerify $gitRepositoryDirectory
 
 if [ $? -eq 0 ]
 then
 	echo "$systemGit: git repository found! now to update"
-	gitFunctionPull $gitRepositoryName
+	gitFunctionPull $gitRepositoryDirectory
 else
 	echo "$systemGit: git repository not found! now to clone"
 	gitFunctionClone $gitRepositoryAddress
